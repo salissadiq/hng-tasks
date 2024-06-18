6c0a6832e1eacfb91ab875f0ff565790ad848d40
salissadiq <salissadiq@gmail.com>
2024-06-18 16:43:05 +0100

    Uninstall ip package

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const PORT = process.env.PORT || 5050;
 
 app.get("/", (request, response) => {
     const clientIP =
-        request.headers["x-real-ip"] || 
-        request.headers["x-forwarded-for"] ||
+       
         request.socket.localAddress;
     response.jsonp({
         ip: clientIP,
-        greeting: `Hello Salscodes your IP adress is ${clientIP}`,
+        greeting: `Hello Salscodes your IP adress is: [${clientIP}]`,
     })
 })
 
