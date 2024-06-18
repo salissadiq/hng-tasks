d899f3310fd5f2853397390fca3fe8a6d51e81dc
salissadiq <salissadiq@gmail.com>
2024-06-18 16:40:03 +0100

    Trying to get client IP using express

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const ip = require("ip");
 const app = express();
+
 const PORT = process.env.PORT || 5050;
 
 app.get("/", (request, response) => {
-    const clientIP = ip.address("" || "public" || "private");
+    const clientIP =
+        request.headers["x-real-ip"] || 
+        request.headers["x-forwarded-for"] ||
+        request.socket.localAddress;
     response.jsonp({
         ip: clientIP,
         greeting: `Hello Salscodes your IP adress is ${clientIP}`,
     })
-})          
+})
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
