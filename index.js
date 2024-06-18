12309e2b639dcd670b0a70a40c4d71eea62574b1
salissadiq <salissadiq@gmail.com>
2024-06-18 16:45:51 +0100

    Add custom header for getting real user IP

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 5050;
 
 app.get("/", (request, response) => {
     const clientIP =
-       
+        request.headers["x-real-ip"] || 
+        request.headers["x-forwarded-for"] ||
         request.socket.localAddress;
     response.jsonp({
         ip: clientIP,
