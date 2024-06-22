efcbb1a5552c01e7913073613014ef44b76a77e2
salissadiq <salissadiq@gmail.com>
2024-06-22 16:11:39 +0100

    Add client location

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.get("/", (request, response) => {
-    const clientIP =
+    response.redirect("/api/hello?visitor_name=Mark");
+})
+
+app.get("/api/hello", async(request, response) => {
+    const client_id =
         request.headers["x-real-ip"] || 
         request.headers["x-forwarded-for"] ||
         request.socket.localAddress;
+     var fetch_res = await fetch(`https://ipapi.co/${request.ip}/json/`);
+    var fetch_data = await fetch_res.json();
+
+    const visitor_name = request.query?.visitor_name || "Mark"
     response.jsonp({
-        ip: clientIP,
-        greeting: `Hello Salscodes your IP adress is: [${clientIP}]`,
+        client_id,
+        location: `Your location is ${fetch_data.region}`,
+        greeting: `Hello, ${visitor_name}`, 
     })
 })
 
