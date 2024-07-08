145059bafee0664425e9925381a5eaba18ba099f
salissadiq <salissadiq@gmail.com>
2024-07-08 10:30:00 +0100

    Delete comment code

--- config/database.js
+++ config/database.js
@@ -7,6 +7,5 @@ const env = process.env.NODE_ENV || "development";
 const config = require("./config")[env]
 
 const sequelize = new Sequelize(config)
-// const sequelize = new Sequelize(`postgresql://postgres.lvvkmgwcflcdvcpauiaq:[YOUR-PASSWORD]@aws-0-eu-west-2.pooler.supabase.com:5432/postgres`)
 
 module.exports = sequelize
